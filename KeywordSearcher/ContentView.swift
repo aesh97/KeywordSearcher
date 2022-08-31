510461f6fcbe7051a8a52915f48f438b9ff7201c
aesh97 <aeshel@u.rochester.edu>
2022-08-31 16:55:50 -0400

    removes Whitespace

--- KeywordSearcher/ContentView.swift
+++ KeywordSearcher/ContentView.swift
@@ -7,37 +7,17 @@
 
 import SwiftUI
 
-
-
 struct TextView: UIViewRepresentable {
-    
     @Binding public var text: NSMutableAttributedString
-    
-    
-    
     class Coordinator: NSObject, UITextViewDelegate {
         var control: TextView
-        
-        
-     
         init(_ control: TextView) {
-            
             self.control = control
-            
-            
-            
         }
-     
         func textViewDidChange(_ textView: UITextView) {
-          
             control.text = NSMutableAttributedString(attributedString: textView.attributedText)
-           
         }
-        
-        
     }
-
-    
     func makeCoordinator() -> TextView.Coordinator {
         Coordinator(self)
     }
@@ -53,41 +33,28 @@ struct TextView: UIViewRepresentable {
         view.attributedText = self.text
         view.textAlignment = .left
         view.adjustsFontForContentSizeCategory = true
-        
         view.delegate = context.coordinator
-        
         return view
     }
     
-    
     func updateUIView(_ uiView: UITextView, context: Context) {
         self.text.addAttribute(NSMutableAttributedString.Key.font, value: UIFont.systemFont(ofSize: 20.0), range: NSRange(location: 0,length: self.text.string.count))
-        
         uiView.attributedText = self.text
         uiView.textAlignment = .left
-     
-    }
-    
-    
+     }
 }
 
 struct ContentView: View {
     @Binding public var document: KeywordSearcherDocument
     @State public var startSearch = ""
     @State public var Hits: Array<NSMutableAttributedString> = Array()
-    
     @State public var String: String = ""
-    
     @State var scaleNow: CGFloat = 1
-    
     @State var showBar = true
        
     var body: some View {
-        
         let views = TextView(text: self.$document.text)
-        
         VStack {
-            
             if showBar {
                 HStack {
                     TextField(
@@ -97,24 +64,16 @@ struct ContentView: View {
                             Hits = document.getHit(search: NSMutableAttributedString(string: startSearch))
                             startSearch = ""
                             var index:Int = 0
-                            
                             for sentence in document.getSentences() {
-                                
-                                
                                 if (Hits.contains(sentence)) {
                                     document.text.addAttribute(NSMutableAttributedString.Key.foregroundColor, value: UIColor(Color.red), range: NSRange(location: index,length: sentence.string.count))
                                     index = index + sentence.string.count
-                                    
-                                    
                                 } else {
                                     document.text.addAttribute(NSMutableAttributedString.Key.foregroundColor, value: UIColor(Color.black), range: NSRange(location: index,length: sentence.string.count))
                                     index = index + sentence.string.count
-                                    
                                 }
                             }
-                            
                             index = 0
-                                            
                             views.text = document.text
                             String = document.getText()
                         }
@@ -131,7 +90,6 @@ struct ContentView: View {
                 .frame(minWidth: UIScreen.main.bounds.size.width/4.0, maxWidth: 0.9*UIScreen.main.bounds.size.width)
                 .border(Color.black)
                 .padding()
-                
                 .multilineTextAlignment(.center)
                 .scaledToFill()
                 .scaleEffect(scaleNow)
@@ -139,18 +97,12 @@ struct ContentView: View {
                     MagnificationGesture()
                         .onChanged {scale in scaleNow = scale; showBar = false}
                         .onEnded{_ in scaleNow = scaleNow;
-                            
                             if (scaleNow == 1) {
                                 showBar = true
-                                
                             }
                         }
-                    
-                    
                 )
-            
                 .onTapGesture {
-                    
                     scaleNow = 1
                     showBar = true
                 }
--- KeywordSearcher/KeywordSearcherDocument.swift
+++ KeywordSearcher/KeywordSearcherDocument.swift
@@ -8,17 +8,14 @@
 import SwiftUI
 import UniformTypeIdentifiers
 
-
 extension UTType {
     static var exampleText: UTType {
         UTType(importedAs: "com.example.plain-text")
     }
 }
 extension NSMutableAttributedString {
-    
     func FindAllIndexes(string:NSMutableAttributedString) -> [Int] {
         var counter = 0
-        
         var INDEXES: Array<Int> = Array()
         for char in self.string {
             if (String(char) == string.string) {
@@ -28,10 +25,7 @@ extension NSMutableAttributedString {
         }
         return(INDEXES)
     }
-    
 }
-
-
 
 
 struct KeywordSearcherDocument: FileDocument {
@@ -42,12 +36,8 @@ struct KeywordSearcherDocument: FileDocument {
         for element in array {
             if (element.string.uppercased().contains(search.string.uppercased())) {
                 HitSentences.append(element)
-                
             }
         }
-        
-        
-        
         return(HitSentences)
     }
     
@@ -58,32 +48,19 @@ struct KeywordSearcherDocument: FileDocument {
         test.append(contentsOf: self.text.FindAllIndexes(string: NSMutableAttributedString(string:"?")))
         test.append(contentsOf: self.text.FindAllIndexes(string: NSMutableAttributedString(string:".")))
         test.sort()
-       
         var interArr:Array<NSMutableAttributedString> = Array()
-        
         for i in 1 ..< test.count {
-            
-            
             if (i == 1) {
                 interArr.append(self.text.attributedSubstring(from: NSRange(location: test[i-1], length: test[i]-test[i-1]+1)) as! NSMutableAttributedString)
-                
             } else {
                 interArr.append(self.text.attributedSubstring(from: NSRange(location: test[i-1]+1, length: test[i]-test[i-1])) as! NSMutableAttributedString)
             }
             
-            
-            
-            
         }
-        
-        
-        
+       
         for i in 0 ..< interArr.count {
             array.append(interArr[i])
         }
-        
-        
-        
         
         return(array)
     }
@@ -94,37 +71,22 @@ struct KeywordSearcherDocument: FileDocument {
     }
     
     init(text: NSMutableAttributedString = NSMutableAttributedString(string:"")) {
-        
         self.text = text
-        
     }
-    
-    
-    
     static var readableContentTypes: [UTType] { [.exampleText] }
-    
-    
-    
     init(configuration: ReadConfiguration) throws {
         guard let data = configuration.file.regularFileContents,
               let string = String(data: data, encoding: .utf8)
-        
-        
         else {
             print("yuck")
             print(configuration.contentType)
-           
             throw CocoaError(.fileReadCorruptFile)
-            
-            
         }
         text = NSMutableAttributedString(string: string)
-
     }
     
     func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
         let data = text.string.data(using: .utf8)!
-        
         return .init(regularFileWithContents: data)
     }
 }
